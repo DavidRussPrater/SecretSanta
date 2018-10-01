b9218852ebdd30d4ba5bee980d03464bfbe7c508
DavidRussPrater <davidrussprater@gmail.com>
2018-10-01 15:35:11 -0500

    Cleaned up code and added thorough comments

--- app/src/main/java/com/example/prate/secretsanta/MainActivity.java
+++ app/src/main/java/com/example/prate/secretsanta/MainActivity.java
@@ -1,27 +1,62 @@
 package com.example.prate.secretsanta;
 
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.util.Log;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-        import java.lang.reflect.Array;
-        import java.util.ArrayList;
-        import java.util.Arrays;
-        import java.util.Collections;
-        import java.util.HashMap;
-        import java.util.List;
-        import java.util.Map;
-        import java.util.Random;
-        import java.util.stream.Collectors;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
+    public static HashMap<String, String> createHashMap(ArrayList<String> names) {
+
+        // Create random variable for generating a random int
+        Random random = new Random();
+        // Create a HashMap to store key value pairs for secret santas
+        HashMap<String, String> secretSantaHashMap = new HashMap<>();
+        // Create a copy of the secret santa HashMap
+        HashMap<String, String> reverseHashMap = new HashMap<>();
+        // Create a
+        List<String> namesListCopy = new ArrayList<>(names);
+
+        //For each element in names pass in the element for current name
+        for (String currentName : names) {
+            List<String> namesList = new ArrayList<>(namesListCopy);
+
+            //Remove the current name picked from the names list
+            namesList.remove(currentName);
+            //Remove the current name picked from the reverse HashMap of names
+            namesList.remove(reverseHashMap.get(currentName));
+            //Create a random integer to be used to find a specific index in the names list
+            int randomInt = random.nextInt(namesList.size());
+            // Use the random integer to get a random name at the specified index
+            String randomName = namesList.get(randomInt);
+            // Add the current name and the random name to the secret santa HashMap as key value pairs
+            secretSantaHashMap.put(currentName, randomName);
+            // Add the random name and current name to the reverse HashMap of names
+            reverseHashMap.put(randomName, currentName);
+            // Remove the random name from namesListCopy to prevent a participate from being selected twice
+            namesListCopy.remove(randomName);
+
+        }
+
+        // Return the HashMap of secret santas
+        return secretSantaHashMap;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,73 +65,58 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
-    public static HashMap<String, String> createHashMap(ArrayList<String> names) {
-
-        Random random = new Random();
-        HashMap<String, String> secretSantaHashMap = new HashMap<>();
-        List<String> namesListCopy = new ArrayList<>(names);
-
-        //For each element in names pass in the element
-        for (String currentName : names) {
-            List<String> namesList = new ArrayList<>(namesListCopy);
-
-            namesList.remove(currentName);
-            int randomInt = Math.abs(random.nextInt(namesList.size()));
-            String randomName = namesList.get(randomInt);
-            secretSantaHashMap.put(currentName, randomName);
-            namesListCopy.remove(randomName);
-
-            System.out.println("Random int " + randomInt);
-
-        }
-
-
-        return secretSantaHashMap;
-    }
-
+    /*
+    Create a method to get the names of the participants and add them to an ArrayList
+     */
     public ArrayList<String> createArrayList() {
+        // Find the family members edit text and assign it to familyMembersEditText
         EditText familyMembersEditText = findViewById(R.id.family_members_edit_text);
+        // Get the names from the familyMembersEditText
         String familyMembersNames = familyMembersEditText.getText().toString();
+        // Create an array of names then separate the string of names and add them to array
         String namesArray[] = familyMembersNames.split(", ");
+        // Convert the array of names to and ArrayList of names
         ArrayList<String> names = new ArrayList<>(Arrays.asList(namesArray));
 
         return names;
     }
 
 
-
     public void updateSecretSantaTextView(View view) {
 
         // Initialize a string builder for appending each key value pair with a arrow (key --> value)
-        StringBuilder secretSantaBuilder = new StringBuilder();
+        StringBuilder secretSantaStringBuilder = new StringBuilder();
 
         //Find the secret santa text view and assign it to the variable secret_santa_text_view
         TextView secretSantaTextView = findViewById(R.id.secret_santa_text_view);
-        //Create the variable to update the secret santa text view
+
+        //Create the variable to store the secret santa text
         String secretSantaText;
 
-        //Create a new array list of strings from the names edit text an
+        //Create a new array list of strings to store the names from the secret santa EditText
         ArrayList<String> names = createArrayList();
 
-        //Create secret santa HashMap from the names ArrayList
+        //Create a secret santa HashMap from the names ArrayList
         HashMap<String, String> secretSantaHashMap = createHashMap(names);
 
 
-        // Break the HashMap into it's ke
+        // Created a for loop to create the final string to be displayed int the secret santa TextView
         for (Map.Entry<String, String> entry : secretSantaHashMap.entrySet()) {
+            // Get the key from the secret santa HashMap
             String key = entry.getKey();
+            // Get the value from the secret santa HashMap
             String value = entry.getValue();
 
-            secretSantaBuilder.append(key + " --> " + value + "\n");
+            // Used the string builder and append to create the final string to be displayed
+            secretSantaStringBuilder.append(key + " --> " + value + "\n");
 
         }
 
-        secretSantaText = secretSantaBuilder.toString();
+        // Convert from secret santa string
+        secretSantaText = secretSantaStringBuilder.toString();
 
+        // Set the the final text to be displayed
         secretSantaTextView.setText(secretSantaText);
-
-        //names.clear();
 
     }
 
